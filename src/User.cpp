691527f6db8f2ca3bf45b07396bc13ef39ee5008
endiny <endiny@localhost.localdomain>
2015-05-18 18:13:50 +0300

    first prototype for users. not working

--- src/User.cpp
+++ src/User.cpp
@@ -16,3 +16,26 @@ User::User(const User& orig) {
 User::~User() {
 }
 
+bool User::SignUp(char* msg)
+{
+    std::string temp=msg;
+    std::string word;
+    int space=temp.find_first_of(' ');
+    std::string login=temp.substr(space+1,temp.find_first_of(' ',space+1)-(space+1));
+    space=temp.find_first_of(' ',space+1);
+    f_in.open("users");
+    while (f_in >> word)
+    {
+        if (word==login)
+            return 0;
+    }
+    
+    f_in.close();
+    f_out.open("users", std::ofstream::app | std::ofstream::out);
+    f_out << login << " " << temp.substr(space+1,temp.find_first_of(' ',space+1))
+            << std::endl;
+    f_out.close();
+    return 1;
+    
+    
+}
--- src/User.h
+++ src/User.h
@@ -8,13 +8,26 @@
 #ifndef USER_H
 #define	USER_H
 
+#include <fstream>
+#include <crypt.h>
+#include <string>
+#include <map>
+#include <utility>
+
 class User {
 public:
     User();
     User(const User& orig);
     virtual ~User();
+    bool SignUp(char* msg);
+    bool LogIn();
 private:
-
+    std::pair<std::string,std::string> StrToPair(std::string str);
+    std::map<std::string, std::string> users;
+    
+    const char* seed = "1fb39a31";
+    std::ifstream f_in;
+    std::ofstream f_out;
 };
 
 #endif	/* USER_H */
